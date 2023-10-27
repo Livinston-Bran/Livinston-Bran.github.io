d4d81881d3c705cf7ffdad53a0760062739b143e
Bran Livinston <115134114+Livinston-Bran@users.noreply.github.com>
2023-10-27 15:10:13 +0000

    update

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    behavior: "smooth",
+                    top: targetElement.offsetTop,
+                });
+            }
+        });
+    });
+
+    // Your existing code
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
+
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
-})();
+    });
+});
